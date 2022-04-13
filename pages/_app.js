1f3e01670ee5aa6e7eeaa10e6c9d62a8485837c4
Bigone0617 <xodlfsha@gmail.com>
2022-04-13 11:45:49 +0900

    구글 서치 콘솔 등록

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,7 @@ function MyApp({ Component, pageProps }) {
         <meta property="og:title" content="프론트엔드 개발자 - 김태일 포트폴리오 웹사이트" key="title" />
         <meta property="og:description" content='프론트엔드 개발자 김태일의 포트폴리오 사이트입니다.' key="description" />
         <meta name="viewport" content="target-densitydpi=device-dpi, user-scalable=0, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, width=device-width" />
+        <meta name="google-site-verification" content="fTNuB859nP8iVGGkhCaf5WDYaqSn5vpObazmkC8Q1uI" />
       </Head>
       <span>
         <Component {...pageProps} />
