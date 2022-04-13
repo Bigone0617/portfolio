8475bac30e79c82ce207c6da733dcfd50417785b
Bigone0617 <xodlfsha@gmail.com>
2022-04-13 12:48:43 +0900

    favicon 추가

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,7 @@ function MyApp({ Component, pageProps }) {
         <meta property="og:description" content='프론트엔드 개발자 김태일의 포트폴리오 사이트입니다.' key="description" />
         <meta name="viewport" content="target-densitydpi=device-dpi, user-scalable=0, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, width=device-width" />
         <meta name="google-site-verification" content="fTNuB859nP8iVGGkhCaf5WDYaqSn5vpObazmkC8Q1uI" />
+        <link rel="main icon" href='/favicon.ico'/>
       </Head>
       <span>
         <Component {...pageProps} />
