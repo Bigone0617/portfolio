5e754787c885aba5d542a37b91d2f155b3b6e6a8
Bigone0617 <xodlfsha@gmail.com>
2022-04-14 09:04:30 +0900

    title 추가

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="target-densitydpi=device-dpi, user-scalable=0, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, width=device-width" />
         <meta name="google-site-verification" content="fTNuB859nP8iVGGkhCaf5WDYaqSn5vpObazmkC8Q1uI" />
         <link rel="main icon" href='/favicon.ico'/>
+        <title>프론트엔드 개발자 - 김태일의 포트폴리오</title>
       </Head>
       <span>
         <Component {...pageProps} />
